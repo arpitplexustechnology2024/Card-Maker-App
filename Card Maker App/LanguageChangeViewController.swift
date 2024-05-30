a6b86e1fdabd7fbc7fe710007432ffef1a2cae9f
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-30 09:16:53 -0700

    Commit

---
+++ Card Maker App/LanguageChangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LanguageChangeViewController.swift
+//  Card Maker App
+//
+//  Created by Arpit iOS Dev. on 29/05/24.
+//
+
+import UIKit
+
+class LanguageChangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
